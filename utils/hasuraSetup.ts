5688a13524f67841a2bcfa74cdaea0310551f51b
Rubai-Rahman <rubairahman1@gmail.com>
2023-04-05 14:05:59 +0600

    hasura Permison on jwt completed

--- utils/hasuraSetup.ts
+++ utils/hasuraSetup.ts
@@ -1,21 +1,27 @@
 import axios from "axios";
-import { useSession } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 const baseURL = process.env.hasuraEndPoint;
-
-const hasurasecret: any = process.env.hasuraSecret;
-
 
 const axiosInstance = axios.create({
   baseURL,
   headers: {
     "Content-Type": "application/json",
-    "x-hasura-admin-secret": hasurasecret,
-    
   },
 });
+
+axiosInstance.interceptors.request.use(async (config) => {
+  const session:any = await getSession();
+ 
+  if (session?.jwtToken) {
+    config.headers["Authorization"] = `Bearer ${session.jwtToken}`;
+  }
+  return config;
+});
+
 export const postMutation = async (mutation: any, variables: any) => {
   const response = await axiosInstance.post("", { query: mutation, variables });
   return response.data.data;
 };
+
 export default axiosInstance;
